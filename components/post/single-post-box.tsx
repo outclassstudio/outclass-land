73c1a4300d13f265952f63fb7baff543d64e913a
outclassstudio <mlimshs@gmail.com>
2024-09-30 19:31:03 +0900

    Feat: Set text editor(2)

--- components/post/single-post-box.tsx
+++ components/post/single-post-box.tsx
@@ -4,12 +4,14 @@ import {
   ChatBubbleBottomCenterIcon,
   HandThumbUpIcon,
 } from "@heroicons/react/24/outline";
+import Image from "next/image";
 
 interface PostProps {
   post: {
     id: number;
     title: string;
     description: string | null;
+    photo: string;
     created_at: Date;
     _count: {
       likes: number;
@@ -20,6 +22,9 @@ interface PostProps {
 }
 
 export default function SinglePostBox({ post }: PostProps) {
+  const postPreview = post.description!.split(`\n`)[0];
+  console.log(post.photo);
+
   return (
     <Link
       key={post.id}
@@ -27,6 +32,18 @@ export default function SinglePostBox({ post }: PostProps) {
       className="pb-5 mb-5 border-b border-neutral-700 text-neutral-400
   flex gap-5 last:pb-0 last:border-b-0 items-center"
     >
+      {post.photo ? (
+        <div className="aspect-square w-[120px] relative overflow-hidden">
+          <Image
+            src={`${post.photo}/width=120,height=120`}
+            className="object-cover"
+            fill
+            alt=""
+          />
+        </div>
+      ) : (
+        ""
+      )}
       <div className="flex flex-col gap-2 w-full">
         <h2 className="text-white text-xl font-semibold">{post.title}</h2>
         <p>{post.description}</p>

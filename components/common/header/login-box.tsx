2350446aa4ba27402697524465e7a30aa3312818
outclassstudio <mlimshs@gmail.com>
2024-10-29 14:57:53 +0900

    Fix: CSS

--- components/common/header/login-box.tsx
+++ components/common/header/login-box.tsx
@@ -1,5 +1,5 @@
 import { ChevronRightIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { UserIcon } from "@heroicons/react/24/solid";
+import { QuestionMarkCircleIcon, UserIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
 export default function LoginBox({
@@ -8,25 +8,35 @@ export default function LoginBox({
   handleDropdownOpen: () => void;
 }) {
   return (
-    <div className="flex items-center justify-between border-b pb-3 dark:border-neutral-600">
-      <Link
-        href={"/login"}
-        className="w-full flex gap-3 items-center p-1 hover:bg-neutral-100 rounded-md"
-      >
-        <UserIcon className="size-11 text-neutral-600 dark:text-neutral-400 cursor-pointer" />
-        <div className="flex flex-col w-full">
-          <span className="font-semibold text-neutral-800 dark:text-neutral-200">
-            로그인 해주세요
-          </span>
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-between border-b pb-3 dark:border-neutral-600">
+        <Link
+          href={"/login"}
+          className="w-full flex gap-3 items-center p-1 hover:bg-neutral-100 dark:hover:bg-neutral-800 rounded-md"
+        >
+          <UserIcon className="size-11 text-neutral-600 dark:text-neutral-400 cursor-pointer" />
+          <div className="flex flex-col w-full">
+            <span className="font-semibold text-neutral-800 dark:text-neutral-200">
+              로그인 해주세요
+            </span>
+          </div>
+          <ChevronRightIcon className="size-7 text-neutral-500" />
+        </Link>
+        <div
+          className="p-1 hover:bg-neutral-100 dark:hover:bg-neutral-800 rounded-md cursor-pointer"
+          onClick={handleDropdownOpen}
+        >
+          <XMarkIcon className="size-5 text-neutral-500" />
         </div>
-        <ChevronRightIcon className="size-7 text-neutral-500" />
-      </Link>
-      <div
-        className="p-1 hover:bg-neutral-100 dark:hover:bg-neutral-800 rounded-md cursor-pointer"
-        onClick={handleDropdownOpen}
-      >
-        <XMarkIcon className="size-5 text-neutral-500" />
       </div>
+      <Link
+        href={"/create-account"}
+        className="text-neutral-800 dark:text-neutral-200 font-semibold
+        p-1 hover:bg-neutral-100 dark:hover:bg-neutral-800 rounded-md flex gap-3 items-center"
+      >
+        <QuestionMarkCircleIcon className="size-8 text-neutral-600 dark:text-neutral-400" />
+        계정 만들기
+      </Link>
     </div>
   );
 }
